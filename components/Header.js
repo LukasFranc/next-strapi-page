a383a00f6cc28c7013913c657145ba7167400cf7
Lukáš Franc <lf@antstudio.cz>
2023-01-02 12:33:15 +0100

    change structure and change logo

--- components/Header.js
+++ components/Header.js
@@ -6,10 +6,7 @@ export default function Header() {
         <header className="header">
             <div className="container">
                 <div className="header__logo">
-                    <Image src="/ant-logo-white.svg" alt="ANTstudio" width={218} height={30}/>
-                </div>
-                <div className="contact__info">
-                    <Link href="https://www.antstudio.cz/kontakt" className="btn btn-primary">Kontakt</Link>
+                    <Image src="/ant-logo-color.svg" alt="ANTstudio" width={218} height={30}/>
                 </div>
             </div>
         </header>
